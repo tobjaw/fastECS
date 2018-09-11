29595a8a76d2108494323e2072b5c800072c916a
tobjaw <tobjaw@gmail.com>
2018-09-12 01:52:54 +0200

    feat(demo): RenderSystem better graphics

--- src/demo/index.js
+++ src/demo/index.js
@@ -10,23 +10,41 @@ const entities = []
 const systems = []
 
 const PositionComponent = Component('Position')
+const SizeComponent = Component('Size')
+const ColorComponent = Component('Color')
 
 const Ball1 = Entity({
   id: 'Ball1',
   components: {
     ...PositionComponent({ x: 300, y: 300 }),
+    ...SizeComponent(30),
+    ...ColorComponent('#CC9393'),
   },
 })
-entities.push(Ball1)
+
+const Ball2 = Entity({
+  id: 'Ball2',
+  components: {
+    ...PositionComponent({ x: 200, y: 100 }),
+    ...SizeComponent(25),
+    ...ColorComponent('#8FB28F'),
+  },
+})
+
+entities.push(Ball1, Ball2)
 
 const RenderSystem = System({
   entities,
-  selector: (entity) => entity.components.Position != null,
+  selector: (entity) =>
+    entity.components.Position != null &&
+    entity.components.Size != null &&
+    entity.components.Color != null,
   run: (entities) => {
     ctx.clearRect(0, 0, canvas.width, canvas.height)
     entities.map((entity) => {
       const { x, y } = entity.components.Position
-      const size = 20
+      const size = entity.components.Size
+      ctx.fillStyle = entity.components.Color
       ctx.beginPath()
       ctx.arc(x, y, size / 2, 0, Math.PI * 2, true)
       ctx.closePath()
