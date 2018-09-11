1745b83cec5b898a3f74d18ec135f33c2fba7c0e
tobjaw <tobjaw@gmail.com>
2018-09-12 01:49:48 +0200

    feat(demo): RenderSystem basic graphics

--- src/demo/index.js
+++ src/demo/index.js
@@ -1,6 +1,11 @@
 import { Entity, Component, System } from '..'
 import './style.css'
 
+const WIDTH = 600
+const HEIGHT = 480
+const canvas = document.getElementById('canvas')
+const ctx = canvas.getContext('2d')
+
 const entities = []
 const systems = []
 
@@ -18,12 +23,22 @@ const RenderSystem = System({
   entities,
   selector: (entity) => entity.components.Position != null,
   run: (entities) => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
     entities.map((entity) => {
       const { x, y } = entity.components.Position
-      console.log(`${entity.id} is at ${x},${y}.`)
+      const size = 20
+      ctx.beginPath()
+      ctx.arc(x, y, size / 2, 0, Math.PI * 2, true)
+      ctx.closePath()
+      ctx.fill()
     })
   },
 })
+
 systems.push(RenderSystem)
 
-systems.map((system) => system.run())
+const main = () => {
+  systems.map((system) => system.run())
+  requestAnimationFrame(main)
+}
+main()
