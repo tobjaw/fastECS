f807fb7c52cdd959be5c9f0b77ac6211a752f659
tobjaw <tobjaw@gmail.com>
2018-09-12 01:45:23 +0200

    feat(demo): text RenderSystem add

--- src/demo/index.js
+++ src/demo/index.js
@@ -4,4 +4,26 @@ import './style.css'
 const entities = []
 const systems = []
 
+const PositionComponent = Component('Position')
+
+const Ball1 = Entity({
+  id: 'Ball1',
+  components: {
+    ...PositionComponent({ x: 300, y: 300 }),
+  },
+})
+entities.push(Ball1)
+
+const RenderSystem = System({
+  entities,
+  selector: (entity) => entity.components.Position != null,
+  run: (entities) => {
+    entities.map((entity) => {
+      const { x, y } = entity.components.Position
+      console.log(`${entity.id} is at ${x},${y}.`)
+    })
+  },
+})
+systems.push(RenderSystem)
+
 systems.map((system) => system.run())
